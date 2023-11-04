b2a8b0e4bf562a4dff90e71b6a30c9245df0f998
CAMILO ESTRADA BOTERO <113833877+cestrad5@users.noreply.github.com>
2023-11-04 12:04:51 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var skillsMember = document.getElementById("skillsMember");
+    var skillsMemberValue = skillsMember.value;
+    skills.innerHTML = skillsMemberValue;
+}
